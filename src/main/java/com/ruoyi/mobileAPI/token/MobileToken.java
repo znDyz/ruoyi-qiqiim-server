c65bba48a33c8197d12adc9fb1333db3e164e1a2
znDyz <32562424@qq.com>
2020-03-26 17:48:29 +0800

    修改注释

--- src/main/java/com/ruoyi/mobileAPI/token/MobileToken.java
+++ src/main/java/com/ruoyi/mobileAPI/token/MobileToken.java
@@ -3,6 +3,7 @@ package com.ruoyi.mobileAPI.token;
 import com.ruoyi.framework.security.LoginUser;
 import com.ruoyi.framework.security.service.TokenService;
 import com.ruoyi.framework.web.domain.AjaxResult;
+import com.ruoyi.project.system.domain.SysUser;
 import com.ruoyi.project.system.service.ISysUserService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -27,9 +28,9 @@ public class MobileToken {
     public AjaxResult checkToken(HttpServletRequest request)
     {
         AjaxResult ajax = AjaxResult.success("token验证成功");
-        LoginUser loginUser = tokenService.getLoginUser(request);
+        SysUser sysUser = tokenService.getLoginUser(request).getUser();
         System.out.println("移动端校验token成功!!!!!!!!!!!");
-        ajax.put(AjaxResult.DATA_TAG,loginUser);
+        ajax.put(AjaxResult.DATA_TAG,sysUser);
         return ajax;
     }
 
